79a18e9667610e375c4e3c3bf2685f6957246ee7
Jerry Martin <Agriculturist@users.noreply.github.com>
2019-07-09 14:08:13 -0400

    deleted information pointing nowhere

--- maker.R
+++ maker.R
@@ -8,12 +8,3 @@ library(roxygen2)
 #setwd("")  # be sure to set the working directory to the project folder
 
 roxygen2::roxygenise()  # build the manual
-
-
-setwd("P:/Staff/Martin, Jerry/Software/waterDataSupport")
-install("waterDataSupport")
-
-library(waterDataSupport)
-
-
-# devtools::build_vignettes()
